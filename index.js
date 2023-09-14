b6f7a1c956636790f215bf6cdb7cd43a6f204585
Juani <juaniyparraguirre9@gmaial.com>
2023-09-14 15:18:58 -0300

    TrabaosJsDom

--- index.js
+++ index.js
@@ -72,7 +72,34 @@ const pizzas = [
 // } else {
 //   console.log("Pizza igual o mayor a 600$");
 // }
+// c) El nombre de cada pizza con su respectivo precio.
 
-for (const PreciosDePizzas of pizzas) {
-  console.log(`${PreciosDePizzas.nombre}: $${PreciosDePizzas.precio}`);
-}
+// for (const PreciosDePizzas of pizzas) {
+//   console.log(`${PreciosDePizzas.nombre}: $${PreciosDePizzas.precio}`);
+// }
+
+// d) Todos los ingredientes de cada pizza (En cada iteración imprimir los ingredientes de la pizza que se esta recorriendo). Ayuda: van a tener que realizar dos recorridos, ya que cada pizza del array de pizzas tiene una propiedad "ingredientes" cuyo valor es un array con ingredientes.
+
+// pizzas.forEach((pizza) => {
+//   console.log(`Los ingredientes de la ${pizza.nombre} son:`);
+//   pizza.ingredientes.forEach((ingredientes) => console.log(ingredientes));
+// });
+
+// practica de introduccion de DOM
+// const titulo = document.getElementById(h1);
+
+// const parrafo = document.getElementById(p);
+
+// const caja = document.getElementById(div);
+
+// const texto = document.getElementById(Text);
+
+// const contraseña = document.getElementById(password);
+
+// const correo = document.getElementById(email);
+
+// const cbox = document.getElementById(checkbox);
+
+// const numero = document.getElementById(number);
+
+// console.dir(checkbox);
